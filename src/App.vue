de7373f721c0131c722efb592d1be3cdd1b38ba8
Daniel <dan.fev@hotmail.com>
2025-01-19 19:44:01 -0700

    Creacion de componente creacion de lista de tareas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <ListaDeTareas></ListaDeTareas>
   <contador></contador>
 </template>
 
 <script setup>
 import Contador from "./components/Contador.vue";
+import ListaDeTareas from "./components/ListaDeTareas.vue";
 </script>
 <style scoped>
 p {
